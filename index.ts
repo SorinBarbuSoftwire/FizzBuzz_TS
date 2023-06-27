017d2108628369e0f57ab174729f42adb7805a02
Sorin Barbu <Sorin.Barbu@sofwire.com>
2023-06-27 12:11:35 +0300

    done

--- index.ts
+++ index.ts
@@ -1,36 +1,57 @@
 // This is our main function
 import {resourceLimits} from "worker_threads";
+import {beforeEach} from "node:test";
 
 function fizzbuzz(): void {
     console.log("Hello, World!");
     // Put your code here...
-    let resStr: string;
-    for (let i: number = 1; i <= 100; i++) {
-        resStr = "";
+    let index: number;
+    for (let i: number = 1; i <= 255; i++) {
+        let resStrs: string[] = [];
+        let resStr: string = "";
 
         if (i % 11 === 0) {
-            process.stdout.write("Bong");
-        }
-        else {
-            if (i % 7 !== 0) {
-                process.stdout.write(i.toString() + ": ");
+            resStrs.push("Bong");
+            if (i % 13 === 0) {
+                resStrs.splice(0, 0, "Fezz");
+            }
+        } else {
+            if (i % 7 !== 0 && i % 13 !== 0) {
+                resStr = i.toString() + ": ";
             }
 
             if (i % 3 === 0) {
-                resStr += "Fizz";
+                resStrs.push("Fizz");
             }
             if (i % 5 === 0) {
-                resStr += "Buzz";
+                resStrs.push("Buzz");
             }
             if (i % 7 === 0) {
-                resStr += "Bang"
+                resStrs.push("Bang");
             }
-            if (resStr !== "") {
-                process.stdout.write(resStr);
+            if (i % 13 === 0) {
+                index = -1;
+                for (let i: number = 0; i < resStrs.length; i++) {
+                    if (resStrs[i].indexOf("B") === 0) {
+                        index = i;
+                        break;
+                    }
+                }
+                if (index != -1)
+                    resStrs.splice(index, 0, "Fezz");
+                else
+                    resStrs.push("Fezz");
             }
         }
 
-        process.stdout.write("\n");
+        if (i % 17 == 0)
+            resStrs.reverse();
+
+        for (let j:number = 0; j < resStrs.length; j++) {
+            resStr += resStrs[j]
+        }
+
+        console.log(resStr)
     }
 }
 
