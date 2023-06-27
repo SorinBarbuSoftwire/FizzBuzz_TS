26d92918982a75b9dd261918a44140ef7f0b007b
Sorin Barbu <Sorin.Barbu@sofwire.com>
2023-06-27 10:34:04 +0300

    FizzBuzz 1

--- index.ts
+++ index.ts
@@ -1,7 +1,23 @@
 // This is our main function
+import {resourceLimits} from "worker_threads";
+
 function fizzbuzz(): void {
     console.log("Hello, World!");
     // Put your code here...
+    let resAux: string;
+    for (let i: number = 1; i <= 100; i++) {
+        resAux = "";
+        console.log(i);
+        if (i % 3 == 0) {
+            resAux += "Fizz";
+        }
+        if (i % 5 == 1) {
+            resAux += "Buzz";
+        }
+        if (resAux != "") {
+            console.log(resAux);
+        }
+    }
 }
 
 // Now, we run the main function:
