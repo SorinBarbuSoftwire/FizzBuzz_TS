17efd02d5160e52be4d7dcaf548eaff44dd8b63f
Sorin Barbu <Sorin.Barbu@sofwire.com>
2023-06-27 16:13:29 +0300

    repaired most of the problems

--- index.ts
+++ index.ts
@@ -6,72 +6,79 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-const FIZZ = 0;
-const BUZZ = 1;
-const BANG = 2;
-const BONG = 3;
-const FEZZ = 4;
-const REV = 5;
+enum BuzzWords {
+    FIZZ,
+    BUZZ,
+    BANG,
+    BONG,
+    FEZZ,
+    REV
+}
 
 function fizzbuzz(n:number, rules:number[]): void {
     // Put your code here...
     // initial flags
     let flags: boolean[] = [false, false, false, false, false, false];
     // update flags based on received rules
-    for (let i = 0; i < rules.length; i++) {
-        if (0 <= rules[i] && rules[i] <= 5)
-            flags[rules[i]] = true;
-    }
+    rules.forEach(rule => {
+        if (0 <= rule && rule <= 5)
+            flags[rule] = true;
+    })
 
     for (let i: number = 1; i <= n; i++) {
         let buzzWords: string[] = [];
         let resFinal: string = "";
 
-        if (i % 11 === 0 && flags[BONG]) {
+        if (i % 11 === 0 && flags[BuzzWords.BONG]) {
             buzzWords.push("Bong")
         } else {
-            if (i % 7 === 0 && flags[BANG]) {
-                if (i % 3 === 0 && flags[FIZZ])
+            if (i % 7 === 0 && flags[BuzzWords.BANG]) {
+                if (i % 3 === 0 && flags[BuzzWords.FIZZ]) {
                     buzzWords.push("Fizz");
-                if (i % 5 === 0 && flags[BUZZ])
+                }
+                if (i % 5 === 0 && flags[BuzzWords.BUZZ]) {
                     buzzWords.push("Buzz");
+                }
                 buzzWords.push("Bang");
             } else {
-                if (i % 3 === 0 && flags[FIZZ])
+                if (i % 3 === 0 && flags[BuzzWords.FIZZ]) {
                     buzzWords.push("Fizz");
-                if (i % 5 === 0 && flags[BUZZ])
+                }
+                if (i % 5 === 0 && flags[BuzzWords.BUZZ]) {
                     buzzWords.push("Buzz");
+                }
             }
         }
 
-        if (i % 13 === 0 && flags[FEZZ]) {
+        if (i % 13 === 0 && flags[BuzzWords.FEZZ]) {
             let index:number;
             for (index = 0; index < buzzWords.length; index++) {
-                if (buzzWords[index].indexOf("B") === 0)
+                if (buzzWords[index].indexOf("B") === 0) {
                     break;
+                }
             }
             buzzWords.splice(index, 0, "Fezz");
         }
 
-        if (i % 17 == 0 && flags[REV]) {
+        if (i % 17 == 0 && flags[BuzzWords.REV]) {
             buzzWords.reverse();
         }
 
         // concatenate the buzzwords
-        for (let i: number = 0; i < buzzWords.length; i++) {
-            resFinal += buzzWords[i];
-        }
+        resFinal = buzzWords.join('');
 
         // check if there are any buzzwords
-        if (buzzWords.length === 0)
+        if (buzzWords.length === 0) {
             console.log(i);
-        else
+        }
+        else {
             console.log(resFinal);
+        }
     }
 }
 
-let n: number;
-let rules: number[] = [];
+let count: number;
+const rules: number[] = [];
 rl.question("How many numbers?\n", (answer1) => {
     rl.question("Select buzzwords [y/n]:\nFIZZ?\n", (answer2) => {
         rl.question("BUZZ?\n", (answer3) => {
@@ -79,21 +86,27 @@ rl.question("How many numbers?\n", (answer1) => {
                 rl.question("BONG?\n", (answer5) => {
                     rl.question("FEZZ?\n", (answer6) => {
                         rl.question("REV?\n", (answer7) => {
-                            n = parseInt(answer1);
-                            if (answer2 === "y")
-                                rules.push(FIZZ);
-                            if (answer3 === "y")
-                                rules.push(BUZZ);
-                            if (answer4 === "y")
-                                rules.push(BANG);
-                            if (answer5 === "y")
-                                rules.push(BONG);
-                            if (answer6 === "y")
-                                rules.push(FEZZ);
-                            if (answer7 === "y")
-                                rules.push(REV);
+                            count = parseInt(answer1);
+                            if (answer2 === "y") {
+                                rules.push(BuzzWords.FIZZ);
+                            }
+                            if (answer3 === "y") {
+                                rules.push(BuzzWords.BUZZ);
+                            }
+                            if (answer4 === "y") {
+                                rules.push(BuzzWords.BANG);
+                            }
+                            if (answer5 === "y") {
+                                rules.push(BuzzWords.BONG);
+                            }
+                            if (answer6 === "y") {
+                                rules.push(BuzzWords.FEZZ);
+                            }
+                            if (answer7 === "y") {
+                                rules.push(BuzzWords.REV);
+                            }
 
-                            fizzbuzz(n, rules);
+                            fizzbuzz(count, rules);
                             rl.close();
                         });
                     });
